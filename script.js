5795bd1d053aee3138ec9ae4a22b7f0f0e31cb6e
Artem Mezentsev <artemamway88@gmail.com>
2020-06-05 20:03:06 +0700

    Создание промисов + прелоудер

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 const parametr = window.location.search;
 const username = parametr.substring(10);
 const url = `https://api.github.com/users/${username}`;
+const date = new Date();
+const time = 5000;
 
-fetch(url)
+const getDate = new Promise ((resolve, reject) => {
+  setTimeout(() => date ? resolve(date) : reject('Дата неизвестна'), time);
+});
+const getUrl = new Promise ((resolve, reject) => {
+  setTimeout(() => url ? resolve(url) : reject('Ссылка не найдена'), time);
+});
+
+const preloader = document.querySelector('.load-1');
+const stopPr = new Promise ((resolve, reject) => {
+  setTimeout(() => {
+    preloader ? resolve(preloader.style.display = 'none') : reject('Неверный класс');
+  }, time - 30);
+})
+
+Promise.all([stopPr, getDate, getUrl])
+  .then(([preloader, date, url]) => fetch(url))
   .then(response => {
     if (response.ok)
       return response.json();
@@ -21,13 +38,15 @@ fetch(url)
 
     const title = document.createElement('h2');
     const div = document.createElement('div');
+    const elem = document.createElement('p');
 
     const img = new Image();
     img.src = avatar;
     title.innerHTML = name;
     div.innerHTML = bio;
+    elem.innerHTML = date;
 
-    document.body.append(img, title, div);
+    document.body.append(img, title, div, elem);
 
     title.addEventListener("click", () => window.location.assign(mainLink));
   })
